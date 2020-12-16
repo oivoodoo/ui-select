f46d5b6602f85b6687278347fcf2f57e19501b3a
Alexandr Korsak <alex.korsak@gmail.com>
2020-12-16 16:40:39 +0100

    Revert "Merge pull request #12 from oivoodoo/2020.25/ENG-4028-revert"
    This reverts commit 1d4297a5e2050872dcfd458e86e8f0488f2bc4b0, reversing
    changes made to 12c42e0d14f8475dd420f24e1e1e40702ac76724.

--- src/uiSelectController.js
+++ src/uiSelectController.js
@@ -754,14 +754,17 @@ uis.controller('uiSelectCtrl',
   });
 
   $scope.$watch('$select.activeIndex', function(activeIndex) {
-    if (activeIndex != null)
-      $element.find('input').attr(
-        'aria-activedescendant',
-        'ui-select-choices-row-' + ctrl.generatedId + '-' + activeIndex);
+    if (activeIndex != null) {
+      var id = 'ui-select-choices-row-' + ctrl.generatedId + '-' + activeIndex;
+      $element.find('input').attr('aria-activedescendant', id);
+      $element.find('input').parent().attr('aria-expanded', 'true');
+    }
   });
 
   $scope.$watch('$select.open', function(open) {
-    if (!open)
+    if (!open) {
       $element.find('input').removeAttr('aria-activedescendant');
+      $element.find('input').parent().attr('aria-expanded', 'false');
+    }
   });
 }]);
